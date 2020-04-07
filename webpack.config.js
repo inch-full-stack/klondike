4ab75a306705d550dd954de0ff4280a43d562457
inch-full-stack <inchikololo@gmail.com>
2020-04-07 22:48:03 +0300

    issue #3 - add webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
       hot: true,
       host: "localhost",
       inline: true,
-      contentBase: './src/public',  //source of static assets
+      contentBase: './src/app/assets',  //source of static assets
       port: 7700, // port to run dev-server
       watchContentBase: true,
       watchOptions: {
